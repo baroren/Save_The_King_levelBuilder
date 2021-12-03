1706d1d34d52af9c513b6a0a1d98754ebb02f3a9
Netanel Bondat <netanelbondar74@gmail.com>
2021-12-03 21:04:33 +0200

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+﻿
+
+#include <SFML/Graphics.hpp>
+
+
+int main()
+{
+	auto circle = sf::CircleShape (100);
+	auto window = sf::RenderWindow (sf::VideoMode(500, 500), "Hello There");
+
+	circle.setFillColor(sf::Color::Red);
+
+	circle.setOrigin(circle.getRadius(), circle.getRadius());
+
+	circle.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+
+	while (window.isOpen())
+	{
+		window.clear();
+		window.draw(circle);
+		window.display();
+	}
+}
