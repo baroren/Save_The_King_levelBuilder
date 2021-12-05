b259b6b872762329e48bfa47e2f96774363617e0
Netanel Bondat <netanelbondar74@gmail.com>
2021-12-05 17:10:23 +0200

    fixed image path issues

--- include/Menu.h
+++ include/Menu.h
@@ -18,16 +18,9 @@ private:
 //		array of all display objects of the menu
 	DisplayObject m_buttons[11];
 
-//		paths to images, working only when full path entered, not sure why
-	const vector<string> m_imagesPath = {"C:/מכללה/תכנות מונחה עצמים/תרגיל 4/ex04_oop/images/king_image.png", 
-										"C:/מכללה/תכנות מונחה עצמים/תרגיל 4/ex04_oop/images/mage_image.png",
-										"C:/מכללה/תכנות מונחה עצמים/תרגיל 4/ex04_oop/images/warrior_image.png",
-										"C:/מכללה/תכנות מונחה עצמים/תרגיל 4/ex04_oop/images/thief_image.png",
-										"C:/מכללה/תכנות מונחה עצמים/תרגיל 4/ex04_oop/images/wall_image.png",
-										"C:/מכללה/תכנות מונחה עצמים/תרגיל 4/ex04_oop/images/gate_image.png",
-										"C:/מכללה/תכנות מונחה עצמים/תרגיל 4/ex04_oop/images/fire_image.png",
-										"C:/מכללה/תכנות מונחה עצמים/תרגיל 4/ex04_oop/images/ork_image1.jpg",
-										"C:/מכללה/תכנות מונחה עצמים/תרגיל 4/ex04_oop/images/key_image.png",
-										"C:/מכללה/תכנות מונחה עצמים/תרגיל 4/ex04_oop/images/teleport_image.png",
-										"C:/מכללה/תכנות מונחה עצמים/תרגיל 4/ex04_oop/images/throne_image.png"};
+//		paths to images
+	const vector<string> m_imagesPath = {"king_image.png", "mage_image.png","warrior_image.png",
+										"thief_image.png", "wall_image.png","gate_image.png",
+										"fire_image.png", "ork_image.jpg", "key_image.png",
+										"teleport_image.png", "throne_image.png"};
 };
